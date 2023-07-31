bbab7c7356a28376bf0d53b3878b444f8e077515
slam-cheg <levashushkov@yandex.ru>
2023-07-31 18:41:46 +0300

    new blocks

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,32 @@
 const employerReviewsSwiper = new Swiper(".employer-reviews__swiper", {
 	loop: false,
-
-	// Navigation arrows
 	navigation: {
-		nextEl: ".employer-reviews__button_next",
-		prevEl: ".employer-reviews__button_prev",
+		nextEl: "#employer-reviews-next-btn",
+		prevEl: "#employer-reviews-prev-btn",
 	},
 	spaceBetween: 40,
 	slidesPerView: 1.2,
 	slideToClickedSlide: true,
 });
+
+const freeMaterialsSwiper = new Swiper(".free-materials__swiper", {
+	loop: false,
+	navigation: {
+		nextEl: "#free-materials-prev-btn",
+		prevEl: "#free-materials-next-btn",
+	},
+	spaceBetween: 20,
+	slidesPerView: 3,
+	slideToClickedSlide: true,
+});
+
+const reviewsSwiper = new Swiper(".reviews__swiper", {
+	loop: false,
+	navigation: {
+		nextEl: "#reviews-next-btn",
+		prevEl: "#reviews-prev-btn",
+	},
+	spaceBetween: 40,
+	slidesPerView: 1.2,
+	slideToClickedSlide: true,
+})
